ff462354b2af1844f1573d3a962a25f0b95e4bbb
Guna18527 <163076622+Guna18527@users.noreply.github.com>
2024-04-26 20:08:02 +0530

    Update static.py

--- newproject/members/templates/static.py
+++ newproject/members/templates/static.py
@@ -1,7 +1,11 @@
 my_django_project/
 ├── my_django_app/
-│   ├── ...
-├── static/
-│   └── index.html
+│   ├── __init__.py
+│   ├── settings.py
+│   ├── urls.py
+│   └── wsgi.py
+├── Dockerfile
+├── requirements.txt
 ├── manage.py
-└── ...
+└── static/
+    └── index.html
